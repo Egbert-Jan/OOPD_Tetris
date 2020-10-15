f401e955f87402a708b9ef721904c33e3a5608d7
Egbert-Jan <terpstra@d-tt.nl>
2020-10-15 13:47:54 +0200

    Initial commit

---
+++ src/Tetris.java
@@ -0,0 +1,23 @@
+import nl.han.ica.oopg.engine.GameEngine;
+
+public class Tetris extends GameEngine {
+
+    public static void main(String[] args) {
+        Tetris main = new Tetris();
+        main.runSketch();
+        System.out.println("ja");
+    }
+
+
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
+
+
